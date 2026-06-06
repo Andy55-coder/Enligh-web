cad5115f1f274b19f7ab8968ab1df08817fb6a83
Andy55-coder <jimmy0937505327@gmail.com>
2026-06-06 23:03:17 +0800

    Delete api directory

--- api/index.py
+++
@@ -1,9 +0,0 @@
-# api/index.py
-import sys
-import os
-
-# 將上一層(專案根目錄)加入系統路徑，這樣 Vercel 才能找到您的 app.py
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# 引入您寫好的 app 實例
-from app import app
